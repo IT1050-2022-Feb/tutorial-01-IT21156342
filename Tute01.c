568364a67e2259aa5b2e753aa109786499f79942
G C Piyumal <99630173+IT21156342@users.noreply.github.com>
2022-02-14 07:13:47 +0000

    tute 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
-  
+  int total,mark1,mark2;
+  float avg;
+  printf("enter marks=\n");
+  scanf("%d", &mark1);
+  printf("enter marks=\n");
+  scanf("%d", &mark2);
+  total=mark1+mark2;
+  avg=total/2.0;
+  printf("avg marks :%.2f",avg);
+ 
+
   return 0;
 }
 
